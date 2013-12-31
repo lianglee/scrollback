68b154e14fe9cf16891e998332242c0537238499
lianglee <manactric@gmail.com>
2013-12-31 13:46:14 +0500

    extend

---
+++ views/default/scrollback/extend/view.php
@@ -0,0 +1,10 @@
+<script>
+window.scrollback = {
+  streams: ["<?php echo scrollback_system_id();?>"]  // Change this!
+};
+
+/* DON'T EDIT: */(function(d,s,h,e){e=d.createElement(s);
+e.src=h+'/client.min.js';e.async=1;scrollback.host=h;
+d.getElementsByTagName(s)[0].parentNode.appendChild(e);}
+(document,'script',location.protocol+'//scrollback.io'));
+</script>
